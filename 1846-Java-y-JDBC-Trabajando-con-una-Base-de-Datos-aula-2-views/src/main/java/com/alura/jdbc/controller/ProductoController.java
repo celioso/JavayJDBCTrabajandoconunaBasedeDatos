2d142e6364eab6f7721c28badf2b76a6777b99cd
Mario Celis <celioso1@hotmail.com>
2023-08-24 11:54:18 -0500

    ProductoDAD1

--- 1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ 1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -1,16 +1,17 @@
 package com.alura.jdbc.controller;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+//import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+//import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.alura.jdbc.dao.ProductoDAD;
 import com.alura.jdbc.factory.ConnectionFactory;
 import com.alura.jdbc.modelo.Producto;
 
@@ -89,50 +90,10 @@ public class ProductoController {
 	}
 
     public void guardar(Producto producto) throws SQLException {
-    	ConnectionFactory factory = new ConnectionFactory();
+    	ProductoDAD productoDAD = new ProductoDAD(new ConnectionFactory().recuperaConexion());
     	
-		final Connection con = factory.recuperaConexion();
-		
-		try(con){
-			con.setAutoCommit(false);
-			
-			final PreparedStatement statement = con.prepareStatement(
-					"INSERT INTO PRODUCTO" 
-							+ "(nombre, descripcion, cantidad)"
-							+ " VALUES(?, ?, ?)",
-							Statement.RETURN_GENERATED_KEYS);
-			
-			try (statement){
-				ejecutaRegistro(producto, statement);
-				
-				con.commit();
-				
-				} catch(Exception e) {
-					e.printStackTrace();
-					System.out.println("ROLLBACK de la transacción");
-					con.rollback();
-				}
-			}
+    	productoDAD.guardar(producto);
     }
 
-	private void ejecutaRegistro(Producto producto, PreparedStatement statement)
-			throws SQLException {
-		statement.setString(1, producto.getNombre());
-		statement.setString(2, producto.getDescripcion());
-		statement.setInt(3, producto.getCantidad());
-		
-		statement.execute();
-		
-		final ResultSet resultSet = statement.getGeneratedKeys();
-		
-		try (resultSet){
-			while(resultSet.next()) {
-				producto.setId(resultSet.getInt(1));
-				System.out.println(
-						String.format(
-								"Fue insertado el producto %s",producto));
-			}
-		}
-		
-	}
+	
 }
---
+++ 1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/modelo/Producto.java
@@ -0,0 +1,56 @@
+package com.alura.jdbc.modelo;
+
+public class Producto {
+
+	private Integer id;
+	
+	private String descripcion;
+	
+	private String nombre;
+	
+	private Integer cantidad;
+	
+	public Producto(String nombre, String descripcion, Integer cantidad) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.cantidad = cantidad;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public void setId(int id) {
+		this.id = id;	
+	}	
+	
+	@Override
+	public String toString() {
+		return String.format(
+				"{id: %s, nombre: %s,descripcion: %s, cantidad:%d}",
+				this.id,
+				this.nombre,
+				this.descripcion,
+				this.cantidad);
+	}
+}
